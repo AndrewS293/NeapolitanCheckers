136f4924a4fb3694f960538115a5f8129d935322
Andrew Stinson <a.c.stinson119@gmail.com>
2026-09-10 16:11:20 -0400

    Began database implementation

---
+++ backend/src/main/java/com/checkers/service/UserService.java
@@ -0,0 +1,26 @@
+package com.checkers.service;
+
+import com.checkers.dto.RegisterRequest;
+import com.checkers.model.User;
+import com.checkers.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder) {
+
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    //Hamzeh's section
+
+   
+}
