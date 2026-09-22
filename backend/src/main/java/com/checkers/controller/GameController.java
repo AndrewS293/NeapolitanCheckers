85aaf38530040c9b7d173e145116e6bbe3081db8
LeoT008 <quanglam10800@gmail.com>
2026-09-21 20:01:47 -0400

    fix: update greeting in test.txt from 'larmm' to 'hello'

--- backend/src/main/java/com/checkers/controller/GameController.java
+++ backend/src/main/java/com/checkers/controller/GameController.java
@@ -1,5 +1,6 @@
 package com.checkers.controller;
 
+import com.checkers.database.MoveDAO;
 import com.checkers.websocket.GameRoom;
 import com.checkers.websocket.GameSessionManager;
 import org.springframework.http.HttpStatus;
@@ -83,4 +84,17 @@ public class GameController {
                 )
         );
     }
+    @GetMapping("/test-move")
+public String testMove() {
+
+    MoveDAO.saveMove(
+        1,
+        13,
+        4,
+        "f6",
+        "g5"
+    );
+
+    return "Move sent to Supabase!";
+ }
 }
